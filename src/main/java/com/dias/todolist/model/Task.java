37ff69b709cc35d08cd21ddbf77b15e41ecbaadb
CaiqueJar <caiquedm11@gamil.com>
2023-03-12 14:36:10 -0300

    Project created

---
+++ src/main/java/com/dias/todolist/model/Task.java
@@ -0,0 +1,7 @@
+package com.dias.todolist.model;
+
+public class Task {
+
+    private Long id;
+    private 
+}
---
+++ src/test/java/com/dias/todolist/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dias.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
